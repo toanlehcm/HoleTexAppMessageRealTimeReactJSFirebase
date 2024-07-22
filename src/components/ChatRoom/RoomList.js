485135c6e429fe66a50c1c4450b42b3b5033ba54
Toan Le <thanhtoanhcm1602@gmail.com>
2024-07-22 16:32:02 +0700

    - Create layout room list.

--- src/components/ChatRoom/RoomList.js
+++ src/components/ChatRoom/RoomList.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import { Collapse, Typography } from 'antd'
+import { Collapse, Typography, Button } from 'antd'
+import styled from 'styled-components';
+import { PlusSquareOutlined } from '@ant-design/icons';
 
 const { Panel } = Collapse
 
+const PanelStyled = styled(Panel)`
+  &&& {
+    .ant-collapse-header, p {
+      color: white
+    }
+
+    .ant-collapse-content-box {
+      padding: 0 40px
+    }
+
+    .add-room {
+      color: white;
+      padding: 0;
+    }
+  }
+`;
+
+const LinkStyled = styled(Typography.Link)`
+  display: block;
+  margin-bottom: 5px;
+  color: white
+`;
+
 function RoomList(props) {
+  const handleAddRoom = () => {
+    // Add room
+  }
+
   return (
-    <Collapse defaultActiveKey={['1']}>
-      <Panel header="Danh sách các phòng" key={1}>
-        <Typography.Link>Room 1</Typography.Link>
-        <Typography.Link>Room 2</Typography.Link>
-        <Typography.Link>Room 3</Typography.Link>
-      </Panel>
+    <Collapse ghost defaultActiveKey={['1']}>
+      <PanelStyled header="Danh sách các phòng" key={1}>
+        <LinkStyled>Room 1</LinkStyled>
+        <LinkStyled>Room 2</LinkStyled>
+        <LinkStyled>Room 3</LinkStyled>
+
+        <Button type='text' icon={<PlusSquareOutlined />} className='add-room' onClick={handleAddRoom}>Thêm phòng</Button>
+      </PanelStyled>
     </Collapse>
   );
 }

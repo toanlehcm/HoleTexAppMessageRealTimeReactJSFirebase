ef92e0add67629f47a2806a47cf076f6063cbda0
Toan Le <thanhtoanhcm1602@gmail.com>
2024-07-23 08:52:05 +0700

    - Create AppProvider.js.

---
+++ src/Context/AppProvider.js
@@ -0,0 +1,62 @@
+import React, { createContext, useContext, useMemo, useState } from 'react';
+import { AuthContext } from './AuthProvider';
+import { useFirestore } from '../hooks/useFirestore';
+
+export const AppContext = createContext();
+
+function AppProvider({ children }) {
+  const [isAddRoomVisible, setIsAddRoomVisible] = useState(false);
+  const [isInviteMemberVisible, setIsInviteMemberVisible] = useState(false);
+  const [selectedRoomId, setSelectedRoomId] = useState('');
+
+  const { userData: { uid } } = useContext(AuthContext);
+
+  const roomsCondition = useMemo(() => {
+    return {
+      fieldName: 'members',
+      operator: 'array-contains',
+      compareValue: uid
+    }
+  }, [uid]);
+
+  const rooms = useFirestore('rooms', roomsCondition);
+
+  const selectedRoom = useMemo(() => rooms.find((room) => room.id === selectedRoomId) || {}, [selectedRoomId, rooms])
+
+  const usersCondition = useMemo(() => {
+    return {
+      fieldName: 'uid',
+      operator: 'in',
+      compareValue: selectedRoom.members
+    }
+  }, [selectedRoom.members]);
+
+  const members = useFirestore('users', usersCondition);
+
+  const clearState = () => {
+    setSelectedRoomId('');
+    setIsAddRoomVisible(false);
+    setIsInviteMemberVisible(false);
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        rooms,
+        members,
+        selectedRoom,
+        isAddRoomVisible,
+        setIsAddRoomVisible,
+        selectedRoomId,
+        setSelectedRoomId,
+        isInviteMemberVisible,
+        setIsInviteMemberVisible,
+        clearState
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export default AppProvider;
